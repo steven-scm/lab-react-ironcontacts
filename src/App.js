d84e85028ba98c76bfa8c58a43ac3df292ff1837
Steven <stevenscemama@gmail.com>
2022-01-25 19:52:35 +0100

    up to iteration 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,127 @@
 import logo from './logo.svg';
 import './App.css';
+import contactsOrigin from "./contacts.json";
+import React from "react"
+import { useState } from "react";
+
+// import { useState } from "react";
+
+// const importContactsJSON = JSON.parse(contacts);
+console.log(contactsOrigin);
+// const contacts = JSON.parse(contactsOrigin);
+// console.log(contacts);
+
 
 function App() {
+
+
+  const [contacts, setContacts] = useState(contactsOrigin.slice(0, 6));
+  // console.log("The state: ", contacts);
+
+  console.log("UseState", useState);
+  console.log("UseState(contactsOrigin.slice(0, 6)) ", useState(contactsOrigin.slice(0, 6)));
+  console.log("The state: ", contacts);
+  console.log("The setContacts: ", setContacts);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>IronContacts</h1>
+      {/* <ul>
+        {contacts.map((contact) => {
+          console.log(contact.name);
+          return (
+            <li>
+              <img src={contact.pictureUrl} alt={contact.name} />
+
+              <p>{contact.name}</p>
+
+              <p>{contact.popularity}</p>
+            </li>
+          )
+        })}
+      </ul> */}
+
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => {
+            console.log(contact.name);
+
+            if (contact.wonOscar && !contact.wonEmmy) {
+              return (
+                <tr>
+                  <td><img src={contact.pictureUrl} alt={contact.name} width='300' /></td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity}</td>
+                  <td> 🏆 </td>
+                  <td> </td>;
+
+                </tr>
+              );
+            } if (contact.wonOscar && contact.wonEmmy) {
+              return (
+                <tr>
+                  <td><img src={contact.pictureUrl} alt={contact.name} width='300' /></td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity}</td>
+                  <td> 🏆 </td>
+                  <td> 🏆 </td>;
+                </tr>
+              );
+            } if (contact.wonEmmy) {
+              return (
+                <tr>
+                  <td><img src={contact.pictureUrl} alt={contact.name} width='300' /></td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity}</td>
+                  <td> </td>
+                  <td> 🏆 </td>;
+                </tr>
+              );
+            } if (!contact.wonEmmy && !contact.wonOscar) {
+              return (
+                <tr>
+                  <td><img src={contact.pictureUrl} alt={contact.name} width='300' /></td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity}</td>
+                  <td> </td>
+                  <td> </td>;
+                </tr>
+              );
+            }
+
+
+            // return (
+            //   <tr>
+            //     <td><img src={contact.pictureUrl} alt={contact.name} width='300' /></td>
+            //     <td>{contact.name}</td>
+            //     <td>{contact.popularity}</td>
+            //     {if(contact.wonOscar){
+            //       <td> 🏆 </td>
+            //     }else{
+            //       <td> </td>;
+            //     }}
+            //   </tr>
+            // )
+          })}
+        </tbody>
+      </table>
+
     </div>
-  );
+
+
+
+  )
 }
 
 export default App;
